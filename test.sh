752dd1e12ac3f579d618ad38d00b3325da7da781
catalinii <t_cata_2000@yahoo.com>
2015-01-16 11:26:40 +1000

    remove test.sh

--- test.sh
+++
@@ -1,3 +0,0 @@
-(echo "PLAY rtsp://192.168.128.5/?freq=623.25&msys=dvbc&mtype=128qam&sr=6900&specinv=0&pids=0,16,50,201,301 RTSP/1.0";echo "Transport: RTP/AVP;unicast;client_port=38076-38077";echo "CSeq: 1";printf '\r\n';printf '\r\n';sleep 10) | nc localhost 554
-
-
