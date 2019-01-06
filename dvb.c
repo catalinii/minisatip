38519dd7cf8556fc1c066e2dd18ce4e651ffa427
root <catalinii@gmail.com>
2019-01-06 15:34:36 -0800

    add DMX_SET_SOURCE to dvb.c

--- dvb.c
+++ dvb.c
@@ -42,6 +42,25 @@
 #include "ca.h"
 #include "utils.h"
 
+#ifndef DMX_SET_SOURCE
+/**
+ * DMX_SET_SOURCE and dmx_source enum removed on 4.14 kernel
+ * From Github.com/OpenPLI/enigma2 commit 7996dbb
+**/
+enum dmx_source
+{
+	DMX_SOURCE_FRONT0 = 0,
+	DMX_SOURCE_FRONT1,
+	DMX_SOURCE_FRONT2,
+	DMX_SOURCE_FRONT3,
+	DMX_SOURCE_DVR0 = 16,
+	DMX_SOURCE_DVR1,
+	DMX_SOURCE_DVR2,
+	DMX_SOURCE_DVR3
+};
+#define DMX_SET_SOURCE _IOW('o', 49, enum dmx_source)
+#endif
+
 char *fe_pilot[] =
 { "on", "off", " ", //auto
 		NULL };
