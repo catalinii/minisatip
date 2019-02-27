ef547fee4ac659da0e6785870e65d5cda29fad43
root <root@debian>
2019-02-27 08:34:26 +0100

    fixes decoding troubles

--- src/stream.c
+++ src/stream.c
@@ -1083,7 +1083,7 @@ int read_dmx(sockets *s)
 	if (!send)
 		return 0;
 
-	ad->flush = 0;
+	ad->flush = flush_all;
 	ls = lock_streams_for_adapter(ad->id);
 	adapter_lock(ad->id);
 	process_dmx(s);
