7d139edc91ee4153b24b80d6cdfa8766f0fcfc55
Catalin Toda <catalinii@yahoo.com>
2019-05-22 23:49:06 -0700

    Blacklist all pmts when one of them cannot be decrypted with DDCI, also make sure just one PMT is enabled on ddci at a time

--- src/ddci.c
+++ src/ddci.c
@@ -52,6 +52,7 @@
 int ddci_adapters;
 extern int dvbca_id;
 extern SCA ca[MAX_CA];
+extern SPMT *pmts[];
 
 // basic whitelist and blacklist for SIDs and DDCI adapters
 uint64_t sid_whitelist[65536], sid_blacklist[65536];
@@ -306,6 +307,15 @@ int find_ddci_for_pmt(SPMT *pmt)
 	return -1;
 }
 
+int is_pmt_running(SPMT *pmt)
+{
+	int ddid;
+	int pid = get_mapping_table(pmt->adapter, pmt->stream_pid[0].pid, &ddid, NULL);
+	if (pid == -1)
+		return 0;
+	return ddid;
+}
+
 // determine if the pids from this PMT needs to be added to the virtual adapter, also adds the PIDs to the translation table
 int ddci_process_pmt(adapter *ad, SPMT *pmt)
 {
@@ -319,6 +329,9 @@ int ddci_process_pmt(adapter *ad, SPMT *pmt)
 		// grace time for card decrypting lower than the default grace_time
 		return TABLES_RESULT_OK;
 	}
+
+	if ((ddid = is_pmt_running(pmt)))
+		LOG_AND_RETURN(TABLES_RESULT_ERROR_NORETRY, "%s: pmt %d (master %d) already running on DDCI %d", __FUNCTION__, pmt->id, pmt->master_pmt, ddci);
 
 	LOG("%s: adapter %d, pmt %d, sid %d, blacklist %d, %s", __FUNCTION__, ad->id, pmt->id, pmt->sid, sid_blacklist[pmt->sid], pmt->name);
 	ddid = find_ddci_for_pmt(pmt);
@@ -431,18 +444,33 @@ int ddci_del_pmt(adapter *ad, SPMT *spmt)
 	update_pids(d->id);
 	return 0;
 }
+void blacklist_pmt_for_ddci(SPMT *pmt, int ddid)
+{
+	uint64_t mask = 1 << ddid;
+	if ((sid_whitelist[pmt->sid] & mask) == 0)
+	{
+		sid_blacklist[pmt->sid] |= mask;
+		LOG("PMT %d, sid %d is blacklisted on DD %d", pmt->id, pmt->sid, ddid);
+	}
+}
+
+// once one of the PMTs sent to the CAM cannot be decrypted,
+// blacklist all PMTs that have the same master PMT with the one given as argument
+void blacklist_pmts_for_ddci(SPMT *pmt, int ddid)
+{
+	int i, master = pmt->master_pmt;
+	SPMT *p;
+	for (i = 0; i < MAX_PMT; i++)
+		if ((p = get_pmt(i)) && p->master_pmt == master)
+			blacklist_pmt_for_ddci(pmts[i], ddid);
+}
 
 int ddci_encrypted(adapter *ad, SPMT *pmt)
 {
 	ddci_device_t *d = get_ddci(ad->id);
 	if (d) // only on DDCI adapter we can understand if the channel is encrypted
 	{
-		uint64_t mask = 1 << d->id;
-		if ((sid_whitelist[pmt->sid] & mask) == 0)
-		{
-			sid_blacklist[pmt->sid] |= mask;
-			LOG("PMT %d, sid %d is blacklisted on DD %d", pmt->id, pmt->sid, d->id);
-		}
+		blacklist_pmts_for_ddci(pmt, d->id);
 		return 0;
 	}
 	int ddid, ddpid;
