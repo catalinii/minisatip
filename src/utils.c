a6511b617de7a74db7417abdb74adf965749082d
Catalin Toda <catalinii@gmail.com>
2019-05-27 13:25:40 -0700

    Revert "small printf format fix" as it breaks the stats web page on 32 bit systems where %ld is not 64 bits
    This reverts commit 4f203f34cab360be7821fa4b77b5a68e4b19bbf3.

--- src/utils.c
+++ src/utils.c
@@ -822,7 +822,7 @@ int snprintf_pointer(char *dest, int max_len, int type, void *p,
 		break;
 
 	case VAR_INT64:
-		nb = snprintf(dest, max_len, "%ld",
+		nb = snprintf(dest, max_len, "%jd",
 					  (int64_t)((*(int64_t *)p) * multiplier));
 		break;
 
@@ -1005,13 +1005,13 @@ int get_json_bandwidth(char *buf, int len)
 	mutex_lock(&bw_mutex);
 	strlcatf(buf, len, ptr, "\
 {\n\
-\"bw\":%ld,\n\
-\"tbw\":%ld,\n\
+\"bw\":%jd,\n\
+\"tbw\":%jd,\n\
 \"reads\":%u,\n\
 \"writes\":%u,\n\
 \"fwrites\":%u,\n\
-\"ns_read\":%ld,\n\
-\"tt\":%ld\n\
+\"ns_read\":%jd,\n\
+\"tt\":%jd\n\
 }",
 			 c_bw, c_tbw, c_reads, c_writes, c_failed_writes, c_ns_read, c_tt);
 	mutex_unlock(&bw_mutex);
