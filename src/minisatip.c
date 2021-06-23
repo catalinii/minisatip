1df6831cb2a0e15a25de1f5940955755a5d15380
André Valentin <avalentin@vmh.kalnet.hooya.de>
2021-06-23 22:41:52 +0200

    fix compiler warning in snprintf function

--- src/minisatip.c
+++ src/minisatip.c
@@ -1826,7 +1826,7 @@ int main(int argc, char *argv[]) {
 	if (opts.bind_dev) {
 		struct ifreq ifr;
 		memset(&ifr, 0, sizeof(ifr));
-		snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), opts.bind_dev);
+		snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), "%s", opts.bind_dev);
   	    	if (setsockopt(ssdp, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr, sizeof(ifr)) < 0)
 			LOG("SSDP: Failed to set SO_BINDTODEVICE to %s", opts.bind_dev);
   	    	if (setsockopt(ssdp1, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr, sizeof(ifr)) < 0)
