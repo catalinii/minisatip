0698b22a3e05484cc13023518180e4b5f6742180
9000h <3009073+9000h@users.noreply.github.com>
2021-07-01 04:09:42 +0200

    remove dead code (#870)
    * remove dead code
    
    Variable 'num_enabled_pids' is modified but its new value is never used.
    
    * Update stream.c
    
    Variable 'rlen' is assigned a value that is never used.
    
    * Update stream.c

--- src/stream.c
+++ src/stream.c
@@ -872,7 +872,6 @@ int process_packets_for_stream(streams *sid, adapter *ad) {
     char rtp_buf[16 * (max_iov + 1)];
     int rtp_pos = 0;
     int iiov = 1;
-    int num_enabled_pids = 0;
     int last_rtp_header = 0;
     int64_t rtime = ad->rtime;
     int total_len = 0;
@@ -888,7 +887,6 @@ int process_packets_for_stream(streams *sid, adapter *ad) {
             for (j = 0; j < MAX_STREAMS_PER_PID && p->sid[j] > -1; j++)
                 if (p->sid[j] == st_id) {
                     pids[p->pid] = 1;
-                    num_enabled_pids++;
                 }
         }
 
@@ -991,8 +989,6 @@ int process_dmx(sockets *s) {
 #ifndef DISABLE_TABLES
     pmt_process_stream(ad);
 #endif
-
-    rlen = ad->rlen;
 
     for (i = 0; i < MAX_STREAMS; i++)
         if (st[i] && st[i]->enabled && st[i]->adapter == ad->id)
