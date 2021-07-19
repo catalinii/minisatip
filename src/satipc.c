53937cae10bfc05008a3e6f705d94960e37cc340
9000h <3009073+9000h@users.noreply.github.com>
2021-07-19 22:58:54 +0200

    Update satipc.c

--- src/satipc.c
+++ src/satipc.c
@@ -204,7 +204,7 @@ int satipc_reply(sockets *s) {
     if (rc == 404)
         sip->restart_needed = 1;
 
-//quirk for Gainatech Netstream 4C when fe=1 is in the URL
+//quirk for Gainatech EyeTV Netstream 4C when fe=1 is in the URL
     if (rc == 503)
         if (ad->sys[0] == SYS_DVBC_ANNEX_A || ad->sys[0] == SYS_DVBC2)
             sip->satip_fe = 0;
