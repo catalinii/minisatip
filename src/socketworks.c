4ab7ea9dd13a07bd432c5033aad839ea6d8857ff
Bernhard Fröhlich <decke@bluelife.at>
2022-05-17 18:11:03 +0200

    Add FreeBSD support (#970)
    * Add missing include for struct sockaddr on FreeBSD
    
    cc -Wall -Wno-switch -ggdb -fPIC   -DMINOR=\"1\" -DDISABLE_DVBCSA -DDISABLE_NETCVCLIENT -DDISABLE_DDCI -DDISABLE_T2MI -DNO_BACKTRACE -c minisatip.c -o ../build/minisatip.o
    In file included from minisatip.c:21:
    In file included from ./minisatip.h:6:
    ./socketworks.h:17:21: error: field has incomplete type 'struct sockaddr'
        struct sockaddr sa;
    
    * Support platforms that don't have SO_BINDTODEVICE
    
    cc -Wall -Wno-switch -ggdb -fPIC -I/usr/local/include  -DMINOR=\"\" -DDISABLE_DVBCSA -DDISABLE_NETCVCLIENT -DDISABLE_DDCI -DDISABLE_T2MI -DNO_BACKTRACE -c minisatip.c -o ../build/minisatip.o
    minisatip.c:1857:46: error: use of undeclared identifier 'SO_BINDTODEVICE'
                if (setsockopt(ssdp, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr,
                                                 ^
    minisatip.c:1860:47: error: use of undeclared identifier 'SO_BINDTODEVICE'
                if (setsockopt(ssdp1, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr,
    
    * Use correct get_mac_address() implementation on FreeBSD
    
    Properly treat Linux implementation for get_mac_address() as such. The
    MacOSX implementation works fine for FreeBSD as well.
    
    cc -Wall -Wno-switch -ggdb -fPIC -I/usr/local/include  -DMINOR=\"\" -DDISABLE_DVBCSA -DDISABLE_NETCVCLIENT -DDISABLE_DDCI -DDISABLE_T2MI -DNO_BACKTRACE -c socketworks.c -o ../build/socketworks.o
    socketworks.c:1002:33: error: use of undeclared identifier 'SIOCGIFHWADDR'
                    if (ioctl(sock, SIOCGIFHWADDR, &ifr) == 0)
                                    ^
    socketworks.c:1009:19: error: no member named 'ifr_hwaddr' in 'struct ifreq'
        memcpy(m, ifr.ifr_hwaddr.sa_data, 6);
                  ~~~ ^
    
    * Support platforms that don't have SO_PRIORITY
    
    cc -Wall -Wno-switch -ggdb -fPIC -I/usr/local/include  -DMINOR=\"\" -DDISABLE_DVBCSA -DDISABLE_NETCVCLIENT -DDISABLE_DDCI -DDISABLE_T2MI -DNO_BACKTRACE -c socketworks.c -o ../build/socketworks.o
    socketworks.c:1567:36: error: use of undeclared identifier 'SO_PRIORITY'
        if (setsockopt(id, SOL_SOCKET, SO_PRIORITY, &d, sizeof(d)))

--- src/socketworks.c
+++ src/socketworks.c
@@ -970,7 +970,7 @@ void set_sockets_rtime(int i, int r) {
     if (ss)
         ss->rtime = r;
 }
-#ifndef __APPLE__
+#ifdef __linux__
 
 int get_mac_address(char *mac) {
     struct ifreq ifr;
@@ -1563,9 +1563,13 @@ void set_socket_dscp(int id, int dscp, int prio) {
     if (setsockopt(id, IPPROTO_IP, IP_TOS, &d, sizeof(d)))
         LOG("%s: setsockopt IP_TOS failed", __FUNCTION__);
 
+#if defined(SO_PRIORITY)
     d = prio;
     if (setsockopt(id, SOL_SOCKET, SO_PRIORITY, &d, sizeof(d)))
         LOG("%s: setsockopt SO_PRIORITY failed", __FUNCTION__);
+#else
+    LOG("%s: setsockopt SO_PRIORITY not implemented", __FUNCTION__);
+#endif
 }
 
 void sockets_set_opaque(int id, void *opaque, void *opaque2, void *opaque3) {
--- src/socketworks.h
+++ src/socketworks.h
@@ -3,6 +3,7 @@
 #define MAX_SOCKS 300
 #include "utils.h"
 #include <netinet/in.h>
+#include <sys/socket.h>
 
 typedef int (*socket_action)(void *s);
 typedef int (*read_action)(int, void *, size_t, void *, int *);
