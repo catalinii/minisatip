e5d6ae1205359765ec1c22d2bf55bd62b71029b0
Catalin Toda <catalinii@yahoo.com>
2022-08-06 14:24:52 -0700

    Report that the TP has signal before getting the actual signal

--- src/adapter.c
+++ src/adapter.c
@@ -1330,7 +1330,9 @@ char *describe_adapter(int sid, int aid, char *dad, int ld) {
 
     if (use_ad) {
         if (ad->status < 0) {
-            status = strength = snr = 0;
+            status = 1;
+	    snr = 15;
+	    strength = 255;
         } else {
             strength = ad->strength;
             snr = ad->snr;
