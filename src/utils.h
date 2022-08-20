28d082759a832d163466752482afe025f93898a0
Sam Stenvall <sam.stenvall@nitor.com>
2022-08-20 19:32:36 +0300

    Remove unused function get_index_hash_search

--- src/utils.h
+++ src/utils.h
@@ -92,8 +92,6 @@ int init_utils(char *arg0);
 void _hexdump(char *desc, void *addr, int len);
 uint32_t crc_32(const uint8_t *data, int datalen);
 void _dump_packets(char *message, unsigned char *b, int len, int packet_offset);
-int get_index_hash_search(int start_pos, void *p, int max, int struct_size,
-                          uint32_t key, uint32_t value);
 int buffer_to_ts(uint8_t *dest, int dstsize, uint8_t *src, int srclen, char *cc,
                  int pid);
 void write_buf_to_file(char *file, uint8_t *buf, int len);
