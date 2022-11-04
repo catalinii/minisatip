e0ebbd26134b75444ec95995fbc6f68840a69d3a
dnmnhat <68333160+dnmnhat@users.noreply.github.com>
2022-11-04 22:27:04 +0700

    Update install-minisatip.sh

--- install-minisatip.sh
+++ install-minisatip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /home
 #clone new folder
-git clone https://github.com/dnmnhat/minisatip.git
+git clone https://github.com/dnmnhat/minisatip.git --no-check-certificate
 # move to new folder
 cd minisatip/
 # configure minisatip
