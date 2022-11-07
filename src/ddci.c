8a208493692f6b84e6871639977a1d4175e8580b
catalinii <catalinii@users.noreply.github.com>
2022-11-07 06:56:47 -0800

    Use demux filters for pmts as previously. This fixes PMT processing on vu+ (#1021)
    Co-authored-by: Catalin Toda <catalinii@yahoo.com>

--- src/ddci.c
+++ src/ddci.c
@@ -425,7 +425,7 @@ int ddci_process_pmt(adapter *ad, SPMT *pmt) {
     {
         LOG("Mapping CAT to PMT %d from transponder %d, DDCI transponder %d",
             pmt->id, ad->transponder_id, d->tid)
-        add_pid_mapping_table(ad->id, 1, pmt->id, d, 1); // add pid 1
+        add_pid_mapping_table(ad->id, 1, pmt->id, d, 1);  // add pid 1
         d->cat_processed = 0;
     }
 
