da094546001a7d85a2a8433a296acbf619f4db70
Sam Stenvall <sam.stenvall@nitor.com>
2023-01-29 21:22:31 +0200

    Remove confusing arithmetic and + 1

--- src/ddci.c
+++ src/ddci.c
@@ -1205,10 +1205,11 @@ int ddci_process_cat(int filter, unsigned char *b, int len, void *opaque) {
             continue;
         caid = b[i + 2] * 256 + b[i + 3];
         if (id < MAX_CA_PIDS) {
-            d->capid[id++] = (b[i + 4] & 0x1F) * 256 + b[i + 5];
+            d->capid[id] = (b[i + 4] & 0x1F) * 256 + b[i + 5];
         }
 
-        LOG("CAT pos %d caid %04X, pid %d", id, caid, d->capid[id - 1]);
+        LOG("CAT pos %d caid %04X, pid %d", id, caid, d->capid[id]);
+        id++;
     }
 
     add_cat = 1;
