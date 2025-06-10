a2c9fa6d1eed0174e74d843cd4c2f51800b6c94f
Catalin Toda <catalinii@yahoo.com>
2025-06-10 07:41:07 -0700

    Write explicitly MB instead of bytes for memory usage

--- src/utils/alloc.h
+++ src/utils/alloc.h
@@ -1,5 +1,6 @@
 #ifndef ALLOC_H
 #define ALLOC_H
+#include <stdint.h>
 
 void *malloc1(int a, const char *f, int l, int r);
 void *realloc1(void *p, int a, const char *f, int l, int r);
@@ -10,7 +11,7 @@ int _ensure_allocated(void **x, int struct_size, int pointer_size,
 
 void init_alloc();
 void free_alloc();
-long int get_allocated_memory();
+uint64_t get_allocated_memory();
 
 #define _malloc(a) malloc1(a, __FILE__, __LINE__, 1)
 #define _free(a) free1(a, __FILE__, __LINE__)
