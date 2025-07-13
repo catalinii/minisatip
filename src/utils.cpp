2731a3f03a831451ffad6a6f41488aaee5b304d2
Catalin Toda <catalinii@yahoo.com>
2025-07-13 09:38:03 -0700

    Allow POST for http server

--- src/utils.cpp
+++ src/utils.cpp
@@ -869,8 +869,8 @@ int is_rtsp_response(char *buf, int len) {
 }
 
 int is_http_request(char *buf, int len) {
-    const char *start[] = {"GET ", "HEAD "};
-    return is_rtsp_http_header(buf, len, start, 2);
+    const char *start[] = {"GET ", "HEAD ", "POST "};
+    return is_rtsp_http_header(buf, len, start, 3);
 }
 
 int is_rtsp_request(char *buf, int len) {
