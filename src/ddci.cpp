catalinii/minisatip

commit 3a0d5d6517a27cd7bb605aa89c1622fb9aae1baf

stores pid streams as a set (#1361)
Co-authored-by: Catalin Toda <catalinii@yahoo.com>

src/ddci.cpp
@@ -743,15 +743,7 @@ int ddci_create_pmt(ddci_device_t *d, SPMT *pmt, uint8_t *new_pmt, int pmt_size,
             if (!p) {
                 p = find_pid(pmt->adapter, 8192); // all pids are requested
             }
-            int is_added = 0, j;
-            if (p) {
-                for (j = 0; j < MAX_STREAMS_PER_PID; j++)
-                    if (p->sid[j] >= 0 && p->sid[j] < MAX_STREAMS) {
-                        is_added = 1;
-                        break;
-                    }
-            }
-            if (is_added == 0) {
+            if (p && !p->sid.empty()) {
                 LOGM("%s: adapter %d pid %d not requested by the client",
                      __FUNCTION__, pmt->adapter, stream_pid.pid);
                 continue;

src/adapter.cpp
@@ -286,7 +286,7 @@ int init_hw(int i) {
     }
     memset(ad->buf, 0, opts.adapter_buffer + 1);
     init_dvb_parameters(&ad->tp);
-    mark_pids_deleted(i, -1, NULL);
+    mark_pids_deleted(i, PID_STREAM_ID_UNDEFINED, NULL);
     update_pids(i);
 
     if (!ad->sys[0])
@@ -384,7 +384,7 @@ int close_adapter(int na) {
         ad->dvr, ad->sock, ad->fe_sock);
     // close all streams attached to this adapter
     //	close_streams_for_adapter (na, -1);
-    mark_pids_deleted(na, -1, NULL);
+    mark_pids_deleted(na, PID_STREAM_ID_UNDEFINED, NULL);
     update_pids(na);
     ad->enabled = 0;
     if (ad->close)
@@ -560,17 +560,14 @@ void dump_pids(int aid) {
                     "pids: %d",
                     aid, p->pid_err);
             dh = false;
+
             LOG("pid %d, fd %d, packets %d, d/c/c2 errs %d/%d/%d, flags %d, "
-                "pmt %d, "
-                "filter "
-                "%d, sock %d, p2 %d, sids: %d %d %d %d %d %d %d %d",
+                "pmt %d, filter %d, sock %d, p2 %d, sids: <%s>",
                 p->pids[i].pid, p->pids[i].fd, p->pids[i].packets,
                 p->pids[i].dec_err, p->pids[i].cc_err, p->pids[i].cc_err2,
                 p->pids[i].flags, p->pids[i].pmt, p->pids[i].filter,
-                p->pids[i].sock, p->pids[i].packets2, p->pids[i].sid[0],
-                p->pids[i].sid[1], p->pids[i].sid[2], p->pids[i].sid[3],
-                p->pids[i].sid[4], p->pids[i].sid[5], p->pids[i].sid[6],
-                p->pids[i].sid[7]);
+                p->pids[i].sock, p->pids[i].packets2,
+                iterable_to_string(p->pids[i].sid).c_str());
         }
 }
 
@@ -823,7 +820,7 @@ void close_adapter_for_stream(int sid, int aid, int close_stream) {
     // delete the attached PIDs as well
     if (ad->sid_cnt == 0) {
         ad->master_sid = -1;
-        mark_pids_deleted(aid, -1, NULL);
+        mark_pids_deleted(aid, PID_STREAM_ID_UNDEFINED, NULL);
         if (ad->standby && close_stream)
             ad->standby(ad);
         init_dvb_parameters(&ad->tp);
@@ -952,7 +949,7 @@ void post_tune(adapter *ad) {
                 LOG("Adding pid %d to the list of pids as not explicitly added "
                     "for adapter %d",
                     pid, aid);
-                mark_pid_add(-1, aid, pid);
+                mark_pid_add(PID_STREAM_ID_UNDEFINED, aid, pid);
             }
         }
     }
@@ -1032,54 +1029,32 @@ SPid *find_pid(int aid, int p) {
 }
 
 void mark_pid_deleted(int aid, int sid, int _pid, SPid *p) {
-    int j;
-    int cnt = 0, sort = 0;
     if (!p)
         p = find_pid(aid, _pid);
     if (!p)
         return;
 
-    if (sid == -1) // delete all sids and the pid
+    if (sid == PID_STREAM_ID_UNDEFINED) // delete all sids and the pid
     {
         if (p->flags != PID_STATE_INACTIVE)
             p->flags = PID_STATE_DELETED;
-        for (j = 0; j < MAX_STREAMS_PER_PID; j++)
-            if (sid == -1) // delete all pids if sid = -1
-                p->sid[j] = -1;
+        p->sid.clear();
         return;
     }
     // sid != -1
-    for (j = 0; j < MAX_STREAMS_PER_PID; j++)
-        if (p->sid[j] == sid) // delete all pids where .sid == sid
-        {
-            p->sid[j] = -1;
-            if ((j + 1 < MAX_STREAMS_PER_PID) && (p->sid[j + 1] >= 0))
-                sort = 1;
-        }
-
-    for (j = 0; j < MAX_STREAMS_PER_PID; j++)
-        if (p->sid[j] >= 0)
-            cnt++;
+    p->sid.erase(sid);
+    bool is_empty = p->sid.empty();
     int keep = 0;
 
 #ifndef DISABLE_PMT
-    if (cnt == 0 && p->filter != -1 && p->flags > PID_STATE_INACTIVE)
+    if (is_empty && p->filter != -1 && p->flags > PID_STATE_INACTIVE)
         keep = get_active_filters_for_pid(
             p->filter, aid, _pid,
             FILTER_ADD_REMOVE); // 0 - no filter with type ADD_REMOVE
 #endif
 
-    if ((cnt == 0) && (p->flags != PID_STATE_INACTIVE) && !keep)
+    if (is_empty && (p->flags != PID_STATE_INACTIVE) && !keep)
         p->flags = PID_STATE_DELETED;
-
-    if (sort) {
-        for (j = 0; j < MAX_STREAMS_PER_PID - 1; j++)
-            if (p->sid[j + 1] > p->sid[j]) {
-                int16_t t = p->sid[j];
-                p->sid[j] = p->sid[j + 1];
-                p->sid[j + 1] = t;
-            }
-    }
 }
 
 void mark_pids_deleted(int aid, int sid,
@@ -1118,27 +1093,18 @@ void mark_pids_deleted(int aid, int sid,
 
 int mark_pid_add(int sid, int aid, int _pid) {
     adapter *ad;
-    int k, i;
+    int i;
     ad = get_adapter(aid);
-    int found = 0;
     SPid *p;
     if (!ad)
         return -1;
     // check if the pid already exists, if yes add the sid
     if ((p = find_pid(aid, _pid))) {
         LOG("found already existing pid %d flags %d", _pid, p->flags);
-        for (k = 0; k < MAX_STREAMS_PER_PID; k++)
-            if (p->sid[k] == -1 || p->sid[k] == sid) {
-                if (p->flags == PID_STATE_DELETED)
-                    p->flags = PID_STATE_NEW;
-                p->sid[k] = sid;
-                found = 1;
-                break;
-            }
-        if (!found) {
-            LOG("too many streams for PID %d adapter %d", _pid, aid);
-            return -1;
-        }
+        if (sid != PID_STREAM_ID_UNDEFINED)
+            p->sid.insert(sid);
+        if (p->flags == PID_STATE_DELETED)
+            p->flags = PID_STATE_NEW;
         return 0;
     }
     // add the new pid in a new position
@@ -1146,7 +1112,9 @@ int mark_pid_add(int sid, int aid, int _pid) {
         if (ad->pids[i].flags == PID_STATE_INACTIVE) {
             ad->pids[i].flags = PID_STATE_NEW;
             ad->pids[i].pid = _pid;
-            ad->pids[i].sid[0] = sid;
+            ad->pids[i].sid.clear();
+            if (sid != PID_STREAM_ID_UNDEFINED)
+                ad->pids[i].sid.insert(sid);
             ad->pids[i].pmt = -1;
             ad->pids[i].filter = -1;
             ad->pids[i].sock = -1;
@@ -1250,7 +1218,7 @@ int set_adapter_parameters(int aid, int sid, transponder *tp) {
             return -1;
         }
         ad->do_tune = 1;
-        mark_pids_deleted(aid, -1, NULL);
+        mark_pids_deleted(aid, PID_STREAM_ID_UNDEFINED, NULL);
         if (update_pids(aid)) {
             ad->do_tune = 0;
             return -1;
@@ -1421,9 +1389,7 @@ void sort_pids(int aid) {
             if (p[i].flags == PID_STATE_INACTIVE ||
                 p[i].packets < p[i + 1].packets) {
                 b = 1;
-                memcpy(&pp, &p[i], sizeof(pp));
-                memcpy(&p[i], &p[i + 1], sizeof(pp));
-                memcpy(&p[i + 1], &pp, sizeof(pp));
+                std::swap(p[i], p[i + 1]);
             }
         }
     }

src/adapter.h
@@ -4,7 +4,7 @@
 #include "minisatip.h"
 
 #include <string>
-
+#include <unordered_set>
 typedef struct ca_device ca_device_t;
 
 #define MAX_ADAPTERS 100
@@ -34,35 +34,31 @@ typedef struct ca_device ca_device_t;
 #define MAX_DELSYS 10
 #define MAX_PMT_FOR_ADAPTER 255
 
-#define MAX_STREAMS_PER_PID 16
 #define MAX_PIDS 128
+#define PID_STREAM_ID_UNDEFINED -1
 #define PID_STATE_INACTIVE 0
 #define PID_STATE_ACTIVE 1
 #define PID_STATE_NEW 2
 #define PID_STATE_DELETED 3
 
 typedef struct struct_pid {
-    int16_t pid;         // pid for this demux - not used
-    int fd;              // fd for this demux
-    int cc_err, cc_err2; // counter errors
-    int16_t sid[MAX_STREAMS_PER_PID];
-    char flags; // 0 - disabled , 1 enabled, 2 - will be enabled next tune when
-                // tune is called, 3 disable when tune is called
+    int16_t pid; // pid for this demux - not used
+    int16_t pmt, filter;
+    char flags;                 // PID_STATE...
+    int fd;                     // fd for this demux
+    int cc_err, cc_err2;        // counter errors
     uint32_t packets, packets2; // how many packets for this pid arrived, used
                                 // to sort the pids
     int dec_err;                // decrypt errors, continuity counters
     uint8_t is_decrypted;       // Set when first decrypted
-    int16_t pmt, filter;
     int16_t cc, cc1, cc2;
     int sock; // sock_id
 #ifdef CRC_TS
     uint32_t crc;
     int count;
 #endif
-    struct_pid() : flags(0) {
-        for (auto &x : sid)
-            x = -1;
-    }
+    std::unordered_set<int16_t> sid;
+    bool has_stream(int id) { return sid.count(id) > 0; }
 } SPid;
 
 typedef struct struct_adapter adapter;

src/pmt.cpp
@@ -408,19 +408,20 @@ int set_filter_flags(int id, int flags) {
     if (flags & FILTER_ADD_REMOVE) {
         SPid *p = find_pid(f->adapter, f->pid);
         if (!p)
-            mark_pid_add(-1, f->adapter, f->pid);
+            mark_pid_add(PID_STREAM_ID_UNDEFINED, f->adapter, f->pid);
     } else if (flags == 0) {
         int add_remove =
             get_active_filters_for_pid(f->master_filter, f->adapter, f->pid,
                                        FILTER_ADD_REMOVE | FILTER_PERMANENT);
         if (!add_remove) {
             SPid *p = find_pid(f->adapter, f->pid);
-            if (p && p->flags != PID_STATE_DELETED && p->sid[0] == -1) {
-                mark_pid_deleted(f->adapter, -1, f->pid, p);
+            if (p && p->flags != PID_STATE_DELETED && p->sid.size() == 0) {
+                mark_pid_deleted(f->adapter, PID_STREAM_ID_UNDEFINED, f->pid,
+                                 p);
                 update_pids(f->adapter);
             } else
-                LOGM("pid not found or pid in use by sid %d",
-                     p ? p->sid[0] : -1);
+                LOGM("pid not found or pid in use by %d streams",
+                     p ? p->sid.size() : -1);
         }
     }
     return 0;
@@ -1132,44 +1133,40 @@ void emulate_add_all_pids(adapter *ad) {
     for (i = 0; i < MAX_PIDS; i++)
         if (ad->pids[i].flags == PID_STATE_ACTIVE ||
             ad->pids[i].flags == PID_STATE_NEW) {
-            for (j = 0; j < MAX_STREAMS_PER_PID; j++)
-                if (ad->pids[i].sid[j] >= 0) {
-                    pids.insert(MAKE_KEY(ad->pids[i].pid, ad->pids[i].sid[j]));
-                }
+            for (auto &sid : ad->pids[i].sid)
+                pids.insert(MAKE_KEY(ad->pids[i].pid, sid));
         }
-    for (i = 0; i < MAX_STREAMS_PER_PID; i++)
-        if (p_all->sid[i] >= 0) {
-            int sid = p_all->sid[i];
-            for (j = 0; j < ad->active_pmts; j++) {
-                SPMT *pmt = get_pmt(ad->active_pmt[j]);
-                if (!pmt)
-                    continue;
-                if (!pids.count(MAKE_KEY(pmt->pid, sid)))
-                    mark_pid_add(sid, ad->id, pmt->pid);
+    for (auto &sid : p_all->sid) {
+        for (j = 0; j < ad->active_pmts; j++) {
+            SPMT *pmt = get_pmt(ad->active_pmt[j]);
+            if (!pmt)
+                continue;
+            if (!pids.count(MAKE_KEY(pmt->pid, sid)))
+                mark_pid_add(sid, ad->id, pmt->pid);
 
-                for (const auto &stream_pid : pmt->stream_pids) {
-                    if (pids.count(MAKE_KEY(stream_pid.pid, sid)))
-                        continue;
-                    LOG("%s: adding pid %d sid %d to emulate all pids",
-                        __FUNCTION__, stream_pid.pid, sid);
-                    mark_pid_add(sid, ad->id, stream_pid.pid);
-                }
-            }
-
-            for (auto fpid : EMU_PIDS_ALL_ENFORCED_PIDS_LIST) {
-                if (pids.count(MAKE_KEY(fpid, sid)))
+            for (const auto &stream_pid : pmt->stream_pids) {
+                if (pids.count(MAKE_KEY(stream_pid.pid, sid)))
                     continue;
-                LOG("%s: adding (enforced) pid %d sid %d to emulate all "
-                    "pids",
-                    __FUNCTION__, fpid, sid);
-                mark_pid_add(sid, ad->id, fpid);
-            }
-            if (!ad->drop_encrypted) {
-                LOG("%s: adding (enforced) pid 8191 (NULL) too", __FUNCTION__);
-                if (!pids.count(MAKE_KEY(8191, sid)))
-                    mark_pid_add(sid, ad->id, 8191);
+                LOG("%s: adding pid %d sid %d to emulate all pids",
+                    __FUNCTION__, stream_pid.pid, sid);
+                mark_pid_add(sid, ad->id, stream_pid.pid);
             }
         }
+
+        for (auto fpid : EMU_PIDS_ALL_ENFORCED_PIDS_LIST) {
+            if (pids.count(MAKE_KEY(fpid, sid)))
+                continue;
+            LOG("%s: adding (enforced) pid %d sid %d to emulate all "
+                "pids",
+                __FUNCTION__, fpid, sid);
+            mark_pid_add(sid, ad->id, fpid);
+        }
+        if (!ad->drop_encrypted) {
+            LOG("%s: adding (enforced) pid 8191 (NULL) too", __FUNCTION__);
+            if (!pids.count(MAKE_KEY(8191, sid)))
+                mark_pid_add(sid, ad->id, 8191);
+        }
+    }
 }
 #undef MAKE_KEY
 

src/stream.cpp
@@ -817,7 +817,7 @@ int check_cc(adapter *ad) {
             }
             p->cc = cc;
         }
-        if (!VALID_SID(p->sid[0]))
+        if (p->sid.size() == 0)
             packet_no_sid++;
 #ifdef CRC_TS
         if (p) {
@@ -899,7 +899,7 @@ void check_cc2(adapter *ad) {
 // Locks the used structs in order to avoid race conditions (sockets -> stream
 // -> adapter) Requires the adapter lock to not be held before calling
 int process_packets_for_stream(streams *sid, adapter *ad) {
-    int i, j, st_id = sid->sid;
+    int i, st_id = sid->sid;
     SPid *p;
     uint8_t *b;
     int max_iov = TCP_MAX_IOV;
@@ -935,10 +935,8 @@ int process_packets_for_stream(streams *sid, adapter *ad) {
     for (i = 0; i < MAX_PIDS; i++)
         if (ad->pids[i].flags == PID_STATE_ACTIVE) {
             p = &ad->pids[i];
-            for (j = 0; j < MAX_STREAMS_PER_PID && p->sid[j] > -1; j++)
-                if (p->sid[j] == st_id) {
-                    pids[p->pid] = 1;
-                }
+            if (p->has_stream(st_id))
+                pids[p->pid] = 1;
         }
 
     if (sid->type == STREAM_RTSP_UDP) {
@@ -1355,7 +1353,7 @@ char *get_stream_protocol(int s_id, char *dest, int max_size) {
 
 char *get_stream_pids(int s_id, char *dest, int max_size) {
     int len = 0;
-    int i, j;
+    int i;
     streams *s = get_sid_nw(s_id);
     adapter *ad;
     dest[0] = 0;
@@ -1369,14 +1367,14 @@ char *get_stream_pids(int s_id, char *dest, int max_size) {
         return dest;
 
     for (i = 0; i < MAX_PIDS; i++)
-        if (ad->pids[i].flags == PID_STATE_ACTIVE)
-            for (j = 0; j < MAX_STREAMS_PER_PID; j++)
-                if (ad->pids[i].sid[j] == s_id) {
-                    if (ad->pids[i].pid == 8192)
-                        strlcatf(dest, max_size, len, "all,");
-                    else
-                        strlcatf(dest, max_size, len, "%d,", ad->pids[i].pid);
-                }
+        if (ad->pids[i].flags == PID_STATE_ACTIVE) {
+            if (ad->pids[i].has_stream(s_id)) {
+                if (ad->pids[i].pid == 8192)
+                    strlcatf(dest, max_size, len, "all,");
+                else
+                    strlcatf(dest, max_size, len, "%d,", ad->pids[i].pid);
+            }
+        }
     if (len > 0) {
         len--;
         dest[len] = 0;

src/utils.h
@@ -2,7 +2,10 @@
 #define UTILS_H
 
 #include "utils/logging/logging.h"
+#include <iostream>
 #include <mutex>
+#include <sstream>
+#include <string>
 
 #include <pthread.h>
 #include <stdint.h>
@@ -49,6 +52,18 @@ int is_rtsp_response(char *buf, int len);
 int is_rtsp_request(char *buf, int len);
 int is_http_request(char *buf, int len);
 int is_byte_array_empty(uint8_t *b, int len);
+template <typename Container>
+std::string iterable_to_string(const Container &container,
+                               const std::string &delimiter = ", ") {
+    std::stringstream ss;
+    for (auto it = container.begin(); it != container.end(); ++it) {
+        ss << *it;
+        if (std::next(it) != container.end()) {
+            ss << delimiter;
+        }
+    }
+    return ss.str();
+}
 
 #define dump_packets(message, b, len, packet_offset)                           \
     if (DEFAULT_LOG & opts.debug)                                              \

tests/test_pmt.cpp
@@ -322,14 +322,14 @@ int test_emulate_add_all_pids() {
     for (auto pid : pids) {
         SPid *p = find_pid(ad.id, pid);
         ASSERT_EQUAL(p->pid, pid, "emulate_add_all_pids failed");
-        ASSERT_EQUAL(p->sid[0], 0,
+        ASSERT_EQUAL(p->sid.count(0), 1,
                      "emulate_add_all_pids failed to set first stream");
-        ASSERT_EQUAL(p->sid[1], 1,
+        ASSERT_EQUAL(p->sid.count(1), 1,
                      "emulate_add_all_pids failed to set second stream");
     }
     SPid *p = find_pid(ad.id, 101); // pid 101 should have sid 0, 1. 2
 
-    ASSERT(p->sid[0] == 2 && p->sid[1] == 0 && p->sid[2] == 1,
+    ASSERT(p->has_stream(2) && p->has_stream(0) && p->has_stream(1),
            "Expected 3 sids to be set fo pid 101");
     opts.emulate_pids_all = 0;
     return 0;

commit 7e9bd6f9623524367fea96408297442ecc50c7f7

Fix inverted condition that caused stream PIDs to be missing from DDCI PMT
Broke in https://github.com/catalinii/minisatip/pull/1361

src/ddci.cpp
@@ -743,7 +743,7 @@ int ddci_create_pmt(ddci_device_t *d, SPMT *pmt, uint8_t *new_pmt, int pmt_size,
             if (!p) {
                 p = find_pid(pmt->adapter, 8192); // all pids are requested
             }
-            if (p && !p->sid.empty()) {
+            if (p && p->sid.empty()) {
                 LOGM("%s: adapter %d pid %d not requested by the client",
                      __FUNCTION__, pmt->adapter, stream_pid.pid);
                 continue;